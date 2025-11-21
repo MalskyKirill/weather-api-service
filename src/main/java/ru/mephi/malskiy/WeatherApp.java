47398e1c7c9f0119ee0b4a7509c8b0c6a527a9b7
Kirill Malskiy <malskykirill@yandex.ru>
2025-11-21 17:56:24 +0300

    добавил поддержку spring-boot

--- src/main/java/ru/mephi/malskiy/WeatherApp.java
+++ src/main/java/ru/mephi/malskiy/WeatherApp.java
@@ -1,45 +1,13 @@
 package ru.mephi.malskiy;
 
-import ru.mephi.malskiy.enums.Coordinates;
-import ru.mephi.malskiy.service.WeatherService;
 
-import java.util.Scanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 
 public class WeatherApp {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            WeatherService weatherService = new WeatherService();
-
-            while (true) {
-                System.out.println("Введите код города или EX что бы выйти");
-                System.out.println("Доступные города: Москва(MSC), Санкт-Петербург(SPB)");
-                System.out.print("> ");
-                String code = scanner.nextLine().trim().toUpperCase();
-
-                if (code.equals("EX")) {
-                    break;
-                }
-
-                Coordinates coordinates = Coordinates.findByCode(code);
-                if (coordinates == null) {
-                    System.out.println("Сервис пока не предоставляет информацию по данному городу");
-                    continue;
-                }
-
-                System.out.println("Введите количество дней в прогнозе, включая текущий");
-                int lim = scanner.nextInt();
-                scanner.nextLine();
-
-                if (lim < 1 || lim > 11) {
-                    System.out.println("Количество дней не может быть меньше 1 и больше 11");
-                    continue;
-                }
-
-                weatherService.getWeather(coordinates.getLocation(), lim);
-            }
-        } catch (Exception e){
-            System.out.println("Ошибка работы программы " + e.getMessage());
-            e.printStackTrace();
-        }
+        SpringApplication.run(WeatherApp.class, args);
     }
 }
